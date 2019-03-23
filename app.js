495e67f60c5f9e01eb6c6d7fe605d2c4a48e4ac9
Arnab Biswas <arnabdbst.99@gmail.com>
2019-03-23 21:41:03 +0530

    Initializing

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 8080;
+let bodyParser = require('body-parser');
+
+app.use(express.static(__dirname+'/public'));
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',function(req,res){
+    res.send('<h1>Home Page</h1>');
+});
+
+app.listen(port, function(req,res){
+    console.log('Server running on port: '+port);
+});
